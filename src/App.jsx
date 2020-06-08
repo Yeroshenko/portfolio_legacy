b596dca7fe0dbcf06809b15b647555a52734b4ef
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-06-08 21:57:11 +0300

    feat: deleted unnecessary files, add assets and minimal styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/App.sass'
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Hello world</h2>
+    </div>
+  )
+}
+
+export default App
